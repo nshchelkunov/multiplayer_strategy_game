18672e74046093cd78ed27fbe585a3c7b82e7e5c
Nikita Shchelkunov <snoyden@yandex.ru>
2018-06-08 23:57:41 +0300

    Test commit

--- Assets/Scripts/Hex.cs
+++ Assets/Scripts/Hex.cs
@@ -8,9 +8,11 @@ public class Hex : MonoBehaviour {
 	public int x;
 	public int y;
 
+	/*
 	public Hex[] GetNeighbours()
 	{
 		GameObject leftNeighbour = GameObject.Find ("Hex_" + (x - 1) + "_" + y);
 		return null;
 	}
+	*/
 }
